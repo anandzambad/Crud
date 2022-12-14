f63c7cdd1eec70ac8faf2a496ba1c52c609faa55
anandzambad <anandzambed@gmail.com>
2022-12-14 12:45:06 +0530

    Project Completed

--- src/Componants/Read.js
+++ src/Componants/Read.js
@@ -5,65 +5,72 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 const Read = () => {
-    const [data, setData]=useState([])
-    function getData (){
-        axios.get('https://6396d1b786d04c763381a20b.mockapi.io/crud')
-        .then((res)=>{
-          console.log(res.data);
-          setData(res.data)  
-        })
+  const [data, setData] = useState([])
+  function getData() {
+    axios.get('https://6396d1b786d04c763381a20b.mockapi.io/crud')
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data)
+      })
 
-   }
-   function handleDelete(id){
+  }
+  function handleDelete(id) {
     axios.delete(`https://6396d1b786d04c763381a20b.mockapi.io/crud/${id}`)
-    .then(()=>{
+      .then(() => {
         getData()
-    })
-   }
-   const setToLocalStorage=(id, name, email)=>{
-        localStorage.setItem("id", id)
-        localStorage.setItem("name", name)
-        localStorage.setItem("email", email)
-   } 
+      })
+  }
+  const setToLocalStorage = (id, name, email) => {
+    localStorage.setItem("id", id)
+    localStorage.setItem("name", name)
+    localStorage.setItem("email", email)
+  }
 
 
- useEffect (()=>{
+  useEffect(() => {
     getData()
-   },[])
-   
+  }, [])
+
   return (
+    
     <div>
-   <Table striped bordered hover size="sm">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Name</th>
-          <th>Email</th>
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
-      {
-        data.map((eachData)=>{
-            return(
-                <>
+      <div className='d-flex justify-content-between m-2'>
+                <h2>Read Operation</h2>
+                <Link to="/" >
+                <button className='btn btn-info'>Create</button></Link>
+            </div>
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        {
+          data.map((eachData) => {
+            return (
+              <>
+               
                 <tbody>
-        <tr>
-          <td>{eachData.id}</td>
-          <td>{eachData.name}</td>
-          <td>{eachData.email}</td>
-          
-       <td><Link to="/update"> <Button variant="success" onClick={()=>setToLocalStorage(eachData.id, eachData.name, eachData.email)}>Edit</Button></Link></td>
-          <td><Button variant="danger" onClick={()=>handleDelete(eachData.id)}>Delete</Button></td>
-        </tr>
-      </tbody>
-                
-                </>
+                  <tr>
+                    <td>{eachData.id}</td>
+                    <td>{eachData.name}</td>
+                    <td>{eachData.email}</td>
+
+                    <td><Link to="/update"> <Button variant="success" onClick={() => setToLocalStorage(eachData.id, eachData.name, eachData.email)}>Edit</Button></Link></td>
+                    <td><Button variant="danger" onClick={() => handleDelete(eachData.id)}>Delete</Button></td>
+                  </tr>
+                </tbody>
+
+              </>
             )
-        })
+          })
         }
-    </Table>
-  </div>
+      </Table>
+    </div>
   )
 }
 
--- src/Componants/Update.js
+++ src/Componants/Update.js
@@ -1,62 +1,66 @@
 import React from 'react';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { Button, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Update () {
+function Update() {
 
-const [id, setId]= useState(0);
-const [name, setName]= useState("");
-const [email, setEmail]= useState("");
-const navigate= useNavigate();
+  const navigate = useNavigate();
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
-useEffect(() => {
- setId(localStorage.getItem("id"));
- setName(localStorage.getItem("name"));
- setEmail(localStorage.getItem("email"));
-}, [])
 
-function handleUpdate(e){
-  e.preventDefault();
-  axios.put(`https://6396d1b786d04c763381a20b.mockapi.io/crud/${id}`,
-  {
-    name: name,
-    emai: email,
-  })
-  .then(()=>{
-    navigate("/read");
-  });
+  useEffect(() => {
+    setId(localStorage.getItem("id"));
+    setName(localStorage.getItem("name"));
+    setEmail(localStorage.getItem("email"));
+  }, [])
 
-}
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    console.log("id", id);
+    axios.put(`https://6396d1b786d04c763381a20b.mockapi.io/crud/${id}`,
+      {
+        name: name,
+        email: email,
+      })
+      .then(() => {
+        navigate("/read");
+      });
+
+  }
 
 
   return (
- <>
-    <h2>Update</h2>
+    <>
+      <h2>Update</h2>
 
-    <Form className='container-fluid'>
-    <Form.Group className="mb-3" controlId="formBasicPassword">
-           <Form.Label>Name</Form.Label>
-           <Form.Control type="Text" placeholder="Name" value={name}
-            onChange={(e)=>setName(e.target.value)} 
-            />
-            </Form.Group>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
-           <Form.Label>Email address</Form.Label>
-           <Form.Control type="email" placeholder="Enter email" value={email}
-           onChange={(e)=>setEmail(e.target.value)}
-            />
-     </Form.Group>
-     <Button variant="primary" type="update" onClick={handleUpdate}>
-           Update
-    </Button>
-    
-    </Form> 
+      <Form className='container-fluid'>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="Text" placeholder="Name" value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Button variant="primary" type="update" onClick={handleUpdate}>
+          Update
+        </Button>
+        <Link to="/read" >
+        <button className='btn btn-secondary  mx-2'>Back</button></Link>
 
-  </>
-        
+      </Form>
+
+    </>
+
 
   )
 }
